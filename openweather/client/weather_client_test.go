d837c215c63c6b9a049b1b895e371cbfca246a01
Ben Schwartz <benschw@gmail.com>
2014-12-18 18:33:27 -0600

    test(openweather): adding test for a city that can't be found

--- openweather/client/weather_client_test.go
+++ openweather/client/weather_client_test.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"fmt"
+	"github.com/benschw/weather-go/openweather/api"
 	. "gopkg.in/check.v1"
 	"log"
 	"testing"
@@ -17,6 +18,7 @@ type IntTestSuite struct {
 
 var _ = Suite(&IntTestSuite{})
 
+// Find should return weather for a city/state
 func (s *IntTestSuite) TestFind(c *C) {
 	// given
 	client := WeatherClient{}
@@ -29,4 +31,18 @@ func (s *IntTestSuite) TestFind(c *C) {
 
 	c.Assert(cond.Main.Temperature > 0, Equals, true)
 	c.Assert(cond.Weather[0].Description, Not(Equals), "")
+}
+
+// Client should return empty "Conditions" when a state isn't found
+func (s *IntTestSuite) TestFindNotFound(c *C) {
+	// given
+	client := WeatherClient{}
+
+	// when
+	cond, err := client.FindForLocation("Foo", "Bar")
+
+	// then
+	c.Assert(err, Equals, nil)
+
+	c.Assert(cond, DeepEquals, api.Conditions{})
 }
