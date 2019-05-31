b4456c853c05d0b17c643bca53d313c5f9569188
Novikov Bogdan <bogdan.novikov@socialtechnologies.io>
2019-05-31 13:57:34 +0300

    refactor docker compose

--- main.go
+++ main.go
@@ -1,43 +1,26 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"github.com/benschw/weather-go/location"
 	"log"
 	"os"
 )
 
 func main() {
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [arguments] <command> \n", os.Args[0])
-		flag.PrintDefaults()
-	}
-	flag.Parse()
-	cmd := flag.Arg(0)
-
 	bindAddress := os.Getenv("bind")
 	databaseDsn := os.Getenv("database")
 
-	log.Printf("Start's with bindAddress:%s dsn:%s", bindAddress, databaseDsn)
+	log.Printf("bindAddress: %s dsn: %s", bindAddress, databaseDsn)
 
-	// Configure Server
 	s, err := location.NewLocationService(bindAddress, databaseDsn)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	switch cmd {
-	case "help":
-		flag.Usage()
-	case "migrate-db":
-		if err := s.MigrateDb(); err != nil {
-			log.Fatal(err)
-		}
-	default:
-		if err := s.Run(); err != nil {
-			log.Fatal(err)
-		}
+	if err := s.MigrateDb(); err != nil {
+		log.Fatal(err)
 	}
-
+	if err := s.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
